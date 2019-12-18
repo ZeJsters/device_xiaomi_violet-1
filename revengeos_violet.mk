92c68230b039cd1e967ae10cecd891755f50af78
Udit Karode <udit.karode@gmail.com>
2019-12-18 22:18:37 +0000

    violet: go official!
    * Also nuke repeated prop
    
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- revengeos_violet.mk
+++ revengeos_violet.mk
@@ -18,7 +18,7 @@ IS_PHONE := true
 $(call inherit-product, vendor/revengeos/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-IS_PHONE := true
+REVENGEOS_BUILDTYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := revengeos_violet
